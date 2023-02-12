2818b776baf579f10f241b97214f8fa97c697740
AtulyaJaiswal <jaiswalatulya.aj@gmail.com>
2023-02-12 13:17:05 +0530

    server side part

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,11 +26,11 @@ const userSchema = new mongoose.Schema({
     avatar:{
         public_id:{
             type:String,
-            required:true
+            // required:true
         },
         url:{
             type:String,
-            required:true
+            // required:true
         }
     },
     role:{
@@ -40,5 +40,45 @@ const userSchema = new mongoose.Schema({
     resetPasswordToken: String,
     resetPasswordExpire: Date,
 });
+
+
+//PASSWORD HASHING
+userSchema.pre("save", async function(next){
+
+    if(!this.isModified("password")){
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password,10);
+
+});
+
+//JWT TOKEN
+userSchema.methods.getJWTToken = function(){
+    return jwt.sign({id:this._id}, process.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRE,
+    });
+};
+
+//COMPARE PASSWORD
+userSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//GENERATING PASSWORD RESET TOKEN
+userSchema.methods.getResetPasswordToken = function () {
+
+    //GENERATING TOKEN
+    const resetToken = crypto.randomBytes(20).toString("hex"); 
+
+    this.resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    this.resetPasswordExpire = Date.now() + 15*60*1000;
+
+    return resetToken;
+}
 
 module.exports = mongoose.model("User", userSchema);
